7bb535583c54e0ff807302f255d1462bd2dbacfb
aNotSoCleverName <eric0work0email@gmail.com>
2025-07-27 10:37:25 +0700

    Trails.gd: Points are now stored based on distance. When drill first starts moving, leaves bigger trails

--- Trails.gd
+++ Trails.gd
@@ -1,17 +1,45 @@
 extends Line2D
 
-const MAX_POINT_COUNT: int = 20;
+const MIN_DIST_BETWEEN_POINTS: int = 20;
+const MAX_DIST: int = 200;
 
 var _drill: Drill;
 
 func _ready() -> void:
 	self._drill = self.get_parent().find_child("Drill");
 	
+	SignalBus_EndlessRun.drill_change_dock.connect(
+		func (inIsDocked: bool):
+			if (!inIsDocked):
+				return;
+			
+			self.clear_points();
+	)
+	
 	SignalBus_EndlessRun.drill_change_pos.connect(
 		func (inPos: Vector2):
-			var drillHeadOffset: Vector2 = Vector2(60, 0).rotated(self._drill.rotation);
-			self.add_point(inPos + drillHeadOffset);
+			var drillHeadPos: Vector2 = self._GetDrillHeadPos(inPos);
 			
-			if (self.points.size() > self.MAX_POINT_COUNT):
+			if (self.points.size() == 0):
+				var drillTailPos: Vector2 = self._GetDrillTailPos(inPos);
+				self.add_point(drillTailPos);
+				self.add_point(inPos);
+				self.add_point(drillHeadPos);
+			
+			var prevPoint: Vector2 = self.points[self.points.size() - 1];
+			if (prevPoint.distance_to(drillHeadPos) < self.MIN_DIST_BETWEEN_POINTS):
+				return;
+			self.add_point(drillHeadPos);
+			
+			var oldestPoint: Vector2 = self.points[0];
+			if (oldestPoint.distance_to(drillHeadPos) > self.MAX_DIST):
 				self.remove_point(0);
 	)
+
+var drillHeadAndTailOffset: int = 60;
+func _GetDrillHeadPos(inDrillCenterPos: Vector2):
+	var drillHeadOffset: Vector2 = Vector2(self.drillHeadAndTailOffset, 0).rotated(self._drill.rotation);
+	return inDrillCenterPos + drillHeadOffset;
+func _GetDrillTailPos(inDrillCenterPos: Vector2):
+	var drillTailOffset: Vector2 = Vector2(-self.drillHeadAndTailOffset, 0).rotated(self._drill.rotation);
+	return inDrillCenterPos + drillTailOffset;
