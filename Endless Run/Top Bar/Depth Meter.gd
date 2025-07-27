a7c21068f764f5ac305de8dfeddd91a3e5ef5f01
aNotSoCleverName <eric0work0email@gmail.com>
2025-07-27 17:31:19 +0700

    Made depth float

--- Endless Run/Top Bar/Depth Meter.gd
+++ Endless Run/Top Bar/Depth Meter.gd
@@ -2,14 +2,16 @@ extends Label
 
 const SURFACE_Y_COORD: int = 256;
 
-var _depth: int = 0:
+var _depth: float = 0:
 	get:
 		return _depth;
 	set(inValue):
 		_depth = inValue / 100;
-		self.text = "Depth: " + str(_depth) + "m"
+		self.text = "Depth: " + str(_depth).pad_decimals(2) + "m"
 
 func _ready() -> void:
+	self._depth = 0;
+	
 	SignalBus_EndlessRun.drill_change_pos.connect(
 		func (inPos: Vector2):
 			self._depth = inPos.y - self.SURFACE_Y_COORD;
