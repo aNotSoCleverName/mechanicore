75824f93d24410e44d381d1a9079bf76435f7ce3
aNotSoCleverName <eric0work0email@gmail.com>
2025-07-27 13:16:33 +0700

    Trails now uses shader

--- Trails.gd
+++ Trails.gd
@@ -5,8 +5,13 @@ const MAX_DIST: int = 200;
 
 var _drill: Drill;
 
+var _shaderTexture: Texture2D;
+
 func _ready() -> void:
-	self._drill = self.get_parent().find_child("Drill");
+	self._drill = self.find_parent("Endless Run").find_child("Drill");
+	
+	var shaderMaterial: ShaderMaterial = self.material;
+	self._shaderTexture = shaderMaterial.get_shader_parameter("image_texture");
 	
 	SignalBus_EndlessRun.drill_change_dock.connect(
 		func (inIsDocked: bool):
@@ -18,6 +23,8 @@ func _ready() -> void:
 	
 	SignalBus_EndlessRun.drill_change_pos.connect(
 		func (inPos: Vector2):
+			shaderMaterial.set_shader_parameter("drill_pos", inPos);
+			
 			var drillHeadPos: Vector2 = self._GetDrillHeadPos(inPos);
 			
 			if (self.points.size() == 0):
