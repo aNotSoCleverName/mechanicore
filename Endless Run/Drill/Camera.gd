c54b9144c845d1f366dc802b2b65eb628e5afa5c
aNotSoCleverName <eric0work0email@gmail.com>
2025-07-31 11:56:45 +0700

    Optimized camera _process() a little

--- Endless Run/Drill/Camera.gd
+++ Endless Run/Drill/Camera.gd
@@ -6,7 +6,7 @@ var _viewportRect: Rect2;
 func _ready() -> void:
 	self._drill = GlobalProperty_EndlessRun.GetDrillNode(self);
 	self._viewportRect = get_viewport_rect();
+	self.global_position.x = self._viewportRect.size.x / 2;
 
 func _process(_delta) -> void:
-	self.global_position.x = self._viewportRect.size.x / 2;
 	self.global_position.y = self._drill.global_position.y;
