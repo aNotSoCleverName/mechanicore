42eb6ea72c600615ace35b89c5e62c8e92342f96
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-01 18:50:42 +0700

    Fixed bug where item stops being crafted when canceling the first item in queue. Also made the code cleaner

--- Base/Menu/Craft/Queue.gd
+++ Base/Menu/Craft/Queue.gd
@@ -7,14 +7,16 @@ func _on_tree_entered() -> void:
 			newQueuedItem._craftItem = inCraftItem;
 			
 			self.add_child(newQueuedItem);
-			var firstQueuedItem: CraftItemQueued = self.get_child(0);
-			if (firstQueuedItem == newQueuedItem):
-				newQueuedItem.StartCraft();
 	)
+
+var _queueTop: CraftItemQueued;
+func _on_child_order_changed():
+	if (self.get_child_count() == 0):
+		return;
 	
-	SignalBus_Base.craft_finished.connect(
-		func (_inCraftItem: CraftItem):
-			if (self.get_child_count() > 0):
-				var queuedItem: CraftItemQueued = self.get_child(0);
-				queuedItem.StartCraft();
-	)
+	var queuedItem: CraftItemQueued = self.get_child(0);
+	if (self._queueTop == queuedItem):
+		return;
+	
+	self._queueTop = queuedItem;
+	queuedItem.StartCraft();
