0bd4aa28d3123db93a12aa68b33fc94bd55598c4
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-02 11:23:52 +0700

    Scroll container height is now set through editor instead of script. Also adjusted margin

--- Base/Menu/Craft/Queue/Queue ScrollContainer.gd
+++
@@ -1,9 +0,0 @@
-extends ScrollContainer
-
-func _ready():
-	var hScrollBarHeight = self.get_h_scroll_bar().size.y;
-	var marginContainer: MarginContainer = self.get_child(0);
-	self.custom_minimum_size += Vector2(
-		0,
-		hScrollBarHeight + marginContainer.get_theme_constant("margin_top") + marginContainer.get_theme_constant("margin_bottom")
-	);
