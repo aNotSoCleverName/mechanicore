83c1b543ec224dcbaa3d5b1f77781f88023216e8
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-04 15:57:50 +0700

    Fixed bug where stock were not shown properly when selling item

--- Base/Menu/Craft/Craft Item/Stock.gd
+++ Base/Menu/Craft/Craft Item/Stock.gd
@@ -5,20 +5,23 @@ var _base: Base:
 		if (_base == null):
 			_base = self.find_parent("Base");
 		return _base;
+		
+var _craftItemNode: CraftItemNode;
 
 func _ready():
+	var ancestorNode: Node = self.get_parent();
+	while !(ancestorNode is CraftItemNode):
+		ancestorNode = ancestorNode.get_parent();
+	self._craftItemNode = ancestorNode;
+	
 	SignalBus_Base.craft_finished.connect(
 		func (inCraftItem: CraftItem):
-			var ancestorNode: Node = self.get_parent();
-			while !(ancestorNode is CraftItemNode):
-				ancestorNode = ancestorNode.get_parent();
-			var craftItemNode: CraftItemNode = ancestorNode;
-			
-			if (inCraftItem == craftItemNode.craftItem):
+			if (inCraftItem == self._craftItemNode.craftItem):
 				self.text = str(self._base.craftItems[inCraftItem]);
 	)
 	
 	SignalBus_Base.shop_sell.connect(
 		func (inCraftItem: CraftItem):
-			self.text = str(self._base.craftItems[inCraftItem]);
+			if (inCraftItem == self._craftItemNode.craftItem):
+				self.text = str(self._base.craftItems[inCraftItem]);
 	)
