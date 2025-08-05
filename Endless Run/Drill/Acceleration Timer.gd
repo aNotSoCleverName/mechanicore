3c831ebf538f6567956454cbbc22852a73fe39f9
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-05 12:28:17 +0700

    Fixed bug where acceleration was triggered by all keys

--- Endless Run/Drill/Acceleration Timer.gd
+++ Endless Run/Drill/Acceleration Timer.gd
@@ -16,12 +16,15 @@ func _input(event):
 	if (!(event is InputEventKey)):
 		return;
 	
-	if (event.is_pressed()):
-		self.start();
-	elif (event.is_released()):
-		self.stop();
+	if (!event.is_action("ui_down") && !event.is_action("ui_up")):
+		return;
 	
 	if (event.is_action("ui_up")):
 		self._acceleration = -1 * absf(self._acceleration);
 	elif (event.is_action("ui_down")):
 		self._acceleration = absf(self._acceleration);
+	
+	if (event.is_pressed()):
+		self.start();
+	elif (event.is_released()):
+		self.stop();
