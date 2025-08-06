002e7f3b5bc1c8b38f4e79997c1d5985a836bceb
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-06 13:44:59 +0700

    Added money and disabled reputation system for testing

--- Base/Shop/Reputation.gd
+++ Base/Shop/Reputation.gd
@@ -14,20 +14,21 @@ var _reputation: int:
 			SignalBus_Base.game_over.emit();
 
 func _on_tree_entered():
-	SignalBus_Base.shop_sell.connect(
-		func (_inCraftItem: CraftItem):
-			self._reputation += 2;
-	)
-	
-	SignalBus_Base.shop_decline.connect(
-		func ():
-			self._reputation -= 2;
-	)
-	
-	SignalBus_Base.shop_leave_angry.connect(
-		func ():
-			self._reputation -= 5;
-	)
+	pass;
+	#SignalBus_Base.shop_sell.connect(
+		#func (_inCraftItem: CraftItem):
+			#self._reputation += 2;
+	#)
+	#
+	#SignalBus_Base.shop_decline.connect(
+		#func ():
+			#self._reputation -= 2;
+	#)
+	#
+	#SignalBus_Base.shop_leave_angry.connect(
+		#func ():
+			#self._reputation -= 5;
+	#)
 
 func _ready():
 	self._reputation = self.DEFAULT_REPUTATION;
