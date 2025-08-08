57dda1f0ad2214d22311ebbb29b9b71e223af31d
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-08 10:51:20 +0700

    Ore Manager.gd: Cleaned code

--- Endless Run/Ores/Ore Manager.gd
+++ Endless Run/Ores/Ore Manager.gd
@@ -1,8 +1,16 @@
 extends Node2D
 class_name OreManager
 
+enum EPossibleChunkRotation
+{
+	deg0 = 0,
+	deg90 = 90,
+	deg180 = 180,
+	deg270 = 270,
+}
+
 #region Properties
-const ORE_CHUNK_DIR_PATH: String = "res://Endless Run/Ores/Ore Chunk";
+const ORE_CHUNK_DIR_PATH: String = "res://Endless Run/Ores/Ore Chunk/";
 var _oreChunkFilePaths: PackedStringArray = [];
 
 var _drill: Drill;
@@ -17,6 +25,33 @@ static func addToPool(inOre: Ore) -> void:
 static func popFromPool(inOreType: Ore.EOreType) -> Ore:
 	var orePool: Array[Ore] = OreManager.orePools[inOreType];
 	return orePool.pop_back();
+#endregion
+
+#region Private functions
+func _InitOreChunkFilePaths():
+	var dir: DirAccess = DirAccess.open(self.ORE_CHUNK_DIR_PATH);
+	var fileNames: PackedStringArray = dir.get_files();
+	
+	var i: int = 0;
+	while i < fileNames.size():
+		var fileName: String = fileNames[i];
+		if (fileName.ends_with(".tscn")):
+			fileNames[i] = self.ORE_CHUNK_DIR_PATH + fileName;
+			i += 1;
+			continue;
+		
+		fileNames.remove_at(i);
+	self._oreChunkFilePaths = fileNames;
+	
+func _GenerateOreChunk(inTopLeft: Vector2) -> OreChunk:
+	var loadedChunkIndex: int = randi_range(0, self._oreChunkFilePaths.size() - 1);
+	var loadedChunk: OreChunk = load(self._oreChunkFilePaths[loadedChunkIndex]).instantiate();
+	
+	loadedChunk.position = inTopLeft;
+	loadedChunk.rotation_degrees = self.EPossibleChunkRotation.values()[randi_range(0, self.EPossibleChunkRotation.values().size() - 1)];
+	
+	self.add_child(loadedChunk);
+	return loadedChunk;
 #endregion
 
 #region Built-in functions
@@ -74,35 +109,7 @@ func _on_tree_entered() -> void:
 			self._prevLoadedChunkY = 0;
 	)
 	#endregion
-#endregion
-
-#region Private functions
-func _InitOreChunkFilePaths():
-	var dir: DirAccess = DirAccess.open(self.ORE_CHUNK_DIR_PATH);
-	var fileNames: PackedStringArray = dir.get_files();
-	
-	var i: int = 0;
-	while i < fileNames.size():
-		var fileName: String = fileNames[i];
-		if (fileName.ends_with(".tscn")):
-			fileNames[i] = self.ORE_CHUNK_DIR_PATH + "/" + fileName;
-			i += 1;
-			continue;
-		
-		fileNames.remove_at(i);
-	self._oreChunkFilePaths = fileNames;
-	
-var _possibleRotations: Array[int] = [0, 90, 180, 270];
-func _GenerateOreChunk(inTopLeft: Vector2) -> OreChunk:
-	var loadedChunkIndex: int = randi_range(0, self._oreChunkFilePaths.size() - 1);
-	var loadedChunk: OreChunk = load(self._oreChunkFilePaths[loadedChunkIndex]).instantiate();
-	
-	loadedChunk.position = inTopLeft;
-	loadedChunk.rotation_degrees = self._possibleRotations[randi_range(0, self._possibleRotations.size() - 1)];
-	
-	self.add_child(loadedChunk);
-	return loadedChunk;
-#endregion
 
 func _ready() -> void:
 	self._drill = GlobalProperty_EndlessRun.GetDrillNode(self);
+#endregion
