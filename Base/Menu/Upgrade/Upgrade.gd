69c7f0a163bac83383aeb5d854e9125c3afbd17e
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-08 16:56:20 +0700

    Added upgrade description

--- Base/Menu/Upgrade/Upgrade.gd
+++ Base/Menu/Upgrade/Upgrade.gd
@@ -32,3 +32,36 @@ var level: int = 0;
 @export var statChange: Array[float] = [0, 0, 0];	# Size must be = maxLevel
 
 @export var prices: Array[int] = [0, 0, 0];	# Size must be = maxLevel
+
+### Unit to be shown in desc
+### E.g: Max speed is shown in m/s, and max armor doesn't have a unit
+@export var descStatUnit: String;
+
+### Multiplier to be shown in desc
+### E.g: Max speed is calculated by the hundreds (100px/s)
+### Because of this, the statChanges are stored in hundreds too
+### But to make it easier to understand by the player, we show it as m/s
+### If 1m = 100px, then we want to convert 100px/s to 1m/s
+### Meaning, descStatMult should be 1/100
+@export var descStatMult: float = 1;
+
+@export var desc: String:
+	get:
+		var formattedDesc: String = desc;
+		
+		var totalStatChange: float = 0;
+		for i: int in self.level + 1:
+			totalStatChange += self.statChange[i];
+		totalStatChange *= self.descStatMult;
+		formattedDesc = formattedDesc.replace("${totalStatChange}", str(totalStatChange) + self.descStatUnit);
+		
+		var nextLevelStatChange: float = self.statChange[self.level] * self.descStatMult;
+		if (nextLevelStatChange == totalStatChange):
+			formattedDesc = formattedDesc.replace(" ${statChange}", "");
+		else:
+			formattedDesc = formattedDesc.replace(
+				" ${statChange}",
+				" ([color=green]+" + str(nextLevelStatChange) + "[/color]" + self.descStatUnit + ")"
+			);
+		
+		return formattedDesc;
