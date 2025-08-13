0b40a8e54ac488e229f28747147c46f005e56efd
Segart <segart24@gmail.com>
2025-08-13 11:25:39 +0700

    Added BGM & SFX

--- Base/Shop/Order.gd
+++ Base/Shop/Order.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 	SignalBus_Base.shop_customer_leave.connect(
 		func ():
 			timer.stop();
+			SoundList.get_node("Alien-talk-2").play();
 	)
 	
 	SignalBus_Base.shop_make_order.connect(
