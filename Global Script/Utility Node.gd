1e32089207c5466b525ce3794189762ef0d0f661
aNotSoCleverName <eric0work0email@gmail.com>
2025-08-14 17:28:34 +0700

    Increase accuracy for function that calculates node's position in window

--- Global Script/Utility Node.gd
+++ Global Script/Utility Node.gd
@@ -1,9 +1,27 @@
 extends Node
 
 func GetNodeWindowPos(inNode: CanvasItem) -> Vector2:
-	var posInViewport: Vector2 = inNode.get_global_transform().origin;
 	var viewport: Viewport = inNode.get_viewport();
-	return (
-		posInViewport +
-		viewport.get_screen_transform().get_origin()
-	)
+	var subViewportContainer: SubViewportContainer = viewport.get_parent();
+	var camera: Camera2D = viewport.get_camera_2d();
+	
+	var pos: Vector2 = inNode.get_global_transform().origin;
+	
+	if (camera != null && camera.get_canvas_layer_node() == inNode.get_canvas_layer_node()):
+		var cameraTopLeft = camera.position;
+		if (camera.anchor_mode == Camera2D.AnchorMode.ANCHOR_MODE_DRAG_CENTER):
+			cameraTopLeft -= 0.5 * subViewportContainer.size;
+		
+		pos -= cameraTopLeft;
+		pos -= camera.offset;
+	
+	while (subViewportContainer != null):
+		pos += subViewportContainer.global_position;
+		viewport = subViewportContainer.get_viewport();
+		subViewportContainer = viewport.get_parent();
+	
+	var rootNode: Node = get_tree().current_scene;
+	if (rootNode.has_method("get_position")):
+		pos -= rootNode.position;
+	
+	return pos;
